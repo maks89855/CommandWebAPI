39d3551de9ae4ff31d0074f01aab106a95b41835
trasef4 <maks89855@gmail.com>
2023-02-27 20:40:46 +0300

    Add PackageRef EntityFrameworkCore Create CommandsController Modify ApplicationRep & adding DI

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,32 @@
+﻿using CommandWebAPI.Data;
+using CommandWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace CommandWebAPI.Controllers
+{
+    //api/commands
+    [Route("api/commands")] 
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly IApplicationRepository _applicationRepository;
+        public CommandsController(IApplicationRepository applicationRepository)
+        {
+            _applicationRepository = applicationRepository;    
+        }
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandsItems = _applicationRepository.GetAllCommand();
+            return Ok(commandsItems);
+        }
+        //GET api/commands/id
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandsByID = _applicationRepository.GetCommandById(id);
+            return Ok(commandsByID);
+        }
+    }
+}
--- Data/ApplicationRepository.cs
+++ Data/ApplicationRepository.cs
@@ -1,6 +1,24 @@
-﻿namespace CommandWebAPI.Data
+﻿using CommandWebAPI.Models;
+
+namespace CommandWebAPI.Data
 {
-    public class ApplicationRepository
+    public class ApplicationRepository : IApplicationRepository
     {
+        public IEnumerable<Command> GetAllCommand()
+        {
+            var commands = new List<Command>() 
+            {
+                new Command { Id=1, HowTo="", Line="", Platform=""},
+                new Command { Id=2, HowTo="", Line="", Platform=""},
+                new Command { Id=3, HowTo="", Line="", Platform=""},
+                new Command { Id=4, HowTo="", Line="", Platform=""},
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "", Line = "", Platform = "" };
+        }
     }
 }
