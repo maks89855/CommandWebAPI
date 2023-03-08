a59eb5740baf7fa5064fa2bce698d489ccd5ebda
trasef4 <maks89855@gmail.com>
2023-03-08 09:50:34 +0300

    Added automapper and implemented it in the controller

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
-﻿using CommandWebAPI.Data;
+﻿using AutoMapper;
+using CommandWebAPI.Data;
+using CommandWebAPI.Dto;
 using CommandWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections;
@@ -11,22 +13,29 @@ namespace CommandWebAPI.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly IApplicationRepository _applicationRepository;
-        public CommandsController(IApplicationRepository applicationRepository)
+        private readonly IMapper _mapper;
+        public CommandsController(IApplicationRepository applicationRepository, IMapper mapper)
         {
-            _applicationRepository = applicationRepository;    
+            _applicationRepository = applicationRepository;   
+            _mapper = mapper;
         }
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandsItems = _applicationRepository.GetAllCommand();
-            return Ok(commandsItems);
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandsItems));
         }
         //GET api/commands/id
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
+        public ActionResult <CommandReadDto> GetCommandById(int id)
         {
             var commandsByID = _applicationRepository.GetCommandById(id);
-            return Ok(commandsByID);
+            if(commandsByID != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandsByID));
+            }
+            return NotFound();
         }
     }
 }
